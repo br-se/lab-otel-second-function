7bbc6a05bb6e756d23ad57668c9ef833aeeef453
gabrielmarques22 <gabrielfelipemarques@live.com>
2023-09-27 16:01:41 -0300

    Final Version

--- src/main/java/com/example/fn/SecondFunction.java
+++ src/main/java/com/example/fn/SecondFunction.java
@@ -7,99 +7,88 @@
 
 package com.example.fn;
 
+import java.util.List;
+
 import com.fnproject.fn.api.InvocationContext;
-// import io.opentelemetry.context.Context;
+
 import com.fnproject.fn.api.RuntimeContext;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributeKey;
+
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 
 //import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 public class SecondFunction {
 
-    // OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-    // .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-    // .buildAndRegisterGlobal();
+    // The getter will be used for incoming requests
+    TextMapGetter<InvocationContext> getter = new TextMapGetter<>() {
+        @Override
+        public String get(InvocationContext carrier, String key) {
+            if (carrier.getRequestHeaders().get("Fn-Http-H-Traceparent").isPresent()) {
+                return carrier.getRequestHeaders().get("Fn-Http-H-Traceparent").get();
+            }
+            return null;
+        }
 
-    // Tracer tracer = openTelemetry.getTracer("dynatrace-manual-opentel", "1.0.0");
+        @Override
+        public Iterable<String> keys(InvocationContext carrier) {
+            return null;
+        }
+    };
 
-    // TextMapGetter<JSONObject> getter = new TextMapGetter<>() {
-    // @Override
-    // public String get(JSONObject carrier, String key) {
-    // System.out.println("TextMapGetter - " + key);
-
-    // JSONObject carrier_data = (JSONObject) carrier.get("carrier");
-    // String value = carrier_data.get("Value").toString();
-    // JSONObject propagationValue = new JSONObject(value);
-    // String traceparent =
-    // propagationValue.get("Fn-Http-H-Traceparent").toString();
-
-    // if (traceparent != null) {
-    // return traceparent;
-    // }
-    // return null;
-    // }
-
-    // @Override
-    // public Iterable<String> keys(JSONObject carrier) {
-    // return null;
-    // }
-
-    // };
-
-    @WithSpan
     public String handleRequest(RuntimeContext runtimeContext, InvocationContext invocationContext, String input) {
 
-        System.out.println("Debugando context");
-        System.out.println("all headers: " + invocationContext.getRequestHeaders());
-        System.out.println("old header: " + invocationContext.getRequestHeaders().get("Fn-Http-H-Traceparent"));
-        System.out.println("new header: " + invocationContext.getRequestHeaders().get("traceparent"));
+        Context extractedContext = GlobalOpenTelemetry.getPropagators().getTextMapPropagator()
+                .extract(Context.current(), invocationContext, getter);
 
-        // try {
-        // TextMapPropagator propagator =
-        // GlobalOpenTelemetry.getPropagators().getTextMapPropagator();
-        // Context parentContext = propagator.extract(Context.current(),
-        // invocationContext, getter);
-        // Span span = tracer.spanBuilder("process message")
-        // .setParent(parentContext)
-        // .setSpanKind(SpanKind.SERVER)
-        // .startSpan();
+        try (Scope scope = extractedContext.makeCurrent()) {
+            String name = (input == null || input.isEmpty()) ? "world" : input;
 
-        // System.out.println("parentContext: " + parentContext);
+            Tracer tracer = GlobalOpenTelemetry.getTracer("manual", "1.0.1");
+            Span serverSpan = tracer.spanBuilder("OCI Handle Request")
+                    .setSpanKind(SpanKind.SERVER)
+                    .startSpan();
+            serverSpan.setAttribute(SemanticAttributes.HTTP_METHOD, "GET"); // TODO Add attributes
 
-        // try {
-        // // Add the attributes defined in the Semantic Conventions
-        // span.setAttribute("key", "value");
+            try {
+                String responseCatFact = MakeRequest();
+                serverSpan.addEvent("Finished Outgoing Request",
+                        Attributes.of(AttributeKey.stringKey("CatFact"), responseCatFact));
+            } catch (Exception e) {
+                System.out.println(e);
+                serverSpan.setStatus(StatusCode.ERROR, e.getMessage());
 
-        // } finally {
-        // span.end();
-        // }
-
-        System.out.println("Entering Java SecondFunction::handleRequest");
-        System.out.println("Value of input is " + input);
-        String name = (input == null || input.isEmpty()) ? "world" : input;
-
-        System.out.println("Value of name is " + name);
-        System.out.println("Exiting Java SecondFunction::handleRequest");
-
-        try {
-            MakeRequest();
+            } finally {
+                serverSpan.end();
+                System.out.println("Value of name is " + name);
+                System.out.println("Exiting Java SecondFunction::handleRequest");
+                return "SecondFunction: " + name + "!";
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
-
-        return "SecondFunction: " + name + "!";
+        return "SecondFunction: end of the file";
     }
 
+    @WithSpan
     public String MakeRequest() throws Exception {
         // avoid creating several instances, should be singleon
         OkHttpClient client = new OkHttpClient();
